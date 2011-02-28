38ad61a2ec9cdf36afca2405e425b22ffb9de9e7
Michael Koval <koval.michael@gmail.com>
2011-02-27 23:55:57 -0500

    added additional error messages to cd and exit

--- shell.c
+++ shell.c
@@ -15,7 +15,7 @@
 #include <sys/wait.h>
 
 #define ARGNUM  16
-#define BUFSIZE 1024
+#define BUFSIZE 4096
 #define DELIMS  " \t\n"
 #define PROMPT  "% "
 #define TIMEOUT 5
@@ -41,8 +41,11 @@ static int cmd_cd(int argc, char *const *argv)
 		n = getenv("HOME");
 		if (!n)
 			n = "/";
-	} else {
+	} else if (argc == 2) {
 		n = argv[1];
+	} else {
+		fprintf(stderr, "err: too many arguments\n");
+		return -1;
 	}
 	x = chdir(n);
 
@@ -58,10 +61,21 @@ static int cmd_exit(int argc, char *const *argv)
 	if (argc < 2) {
 		exit(0);
 	} else {
-		int ret = 0;
+		int ret;
+
+		if (argc > 3) {
+			fprintf(stderr, "err: too many arguments\n");
+			return -1;
+		}
+
+		ret = 0;
 		sscanf(argv[1], "%d", &ret);
 
 		/* if sscanf fails, we exit with 0 */
+		if (ret != 1) {
+			fprintf(stderr, "war: numeric argument required\n");
+			return -1;
+		}
 		exit(ret);
 	}
 	return 0;
@@ -74,7 +88,7 @@ static int cmd_default(int argc, char *const *argv)
 		/* child */
 		int x = execvp(argv[0], argv);
 		if (x) {
-			fprintf(stderr, "execvp fail.");
+			fprintf(stderr, "err: execvp: %s.\n", strerror(errno));
 			fflush(stderr);
 			exit(1);
 		}
